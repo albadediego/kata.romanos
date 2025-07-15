8f7742f6da14e61a3cf2a743a6b4f11f56b887bb
Alba de Diego <albadediegocorrea@gmail.com>
2025-07-15 21:21:12 +0200

    optimizacion de codigo entero_a_romano

--- main.py
+++ main.py
@@ -26,6 +26,13 @@ decenas = {10:"X", 20:"XX", 30:"XXX", 40:"XL", 50:"L", 60:"LX", 70:"LXX", 80:"LX
 centenas = {100:"C", 200:"CC", 300:"CCC", 400:"CD", 500:"D", 600:"DC", 700:"DCC", 800:"DCCC", 900:"CM"}
 millares = {1000: "M", 2000:"MM", 3000:"MMM"}
 
+dic_entero_a_romano = {
+    1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX",
+    10:"X", 20:"XX", 30:"XXX", 40:"XL", 50:"L", 60:"LX", 70:"LXX", 80:"LXXX", 90:"XC",
+    100:"C", 200:"CC", 300:"CCC", 400:"CD", 500:"D", 600:"DC", 700:"DCC", 800:"DCCC", 900:"CM",
+    1000: "M", 2000:"MM", 3000:"MMM"}
+
+
 class RomanNumberError(Exception):
     pass
 
@@ -36,20 +43,29 @@ def entero_a_romano(numero): #1994
     #print(numero_list)
     valor_romano = ''
 
-    for i in range(0, len(numero_list)): #[1000,900,90,4]
-        if i == 0:
-            numero_list[i] = int(numero_list[i])*1000 #agregar 000 al primer valor
-            valor_romano += millares.get(numero_list[i]) #buscar en el diccionario de millares el valor en romano
-        elif i == 1:
-            numero_list[i] = int(numero_list[i])*100
-            valor_romano += centenas.get(numero_list[i])
-        elif i == 2:
-            numero_list[i] = int(numero_list[i])*10
-            valor_romano += decenas.get(numero_list[i])
-        elif i == 3:
-            numero_list[i] = int(numero_list[i])*1
-            valor_romano += unidades.get(numero_list[i])
+    cont = 0
+    valor_num = 1000
+    while cont < 4:
+        numero_list[cont] = int(numero_list[cont]) * valor_num
+        valor_romano += dic_entero_a_romano.get(numero_list[cont]) #buscar en el diccionario el valor en romano
+        cont += 1
+        valor_num /= 10
 
+    '''
+        for i in range(0, len(numero_list)): #[1000,900,90,4]
+            if i == 0:
+                numero_list[i] = int(numero_list[i])*1000 #agregar 000 al primer valor
+                valor_romano += millares.get(numero_list[i]) #buscar en el diccionario de millares el valor en romano
+            elif i == 1:
+                numero_list[i] = int(numero_list[i])*100
+                valor_romano += centenas.get(numero_list[i])
+            elif i == 2:
+                numero_list[i] = int(numero_list[i])*10
+                valor_romano += decenas.get(numero_list[i])
+            elif i == 3:
+                numero_list[i] = int(numero_list[i])*1
+                valor_romano += unidades.get(numero_list[i])
+    '''
 
 
     #print(numero_list)
