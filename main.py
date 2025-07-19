bf9add63a20eb1a458d618a6743f72516d35f2c8
Alba de Diego <albadediegocorrea@gmail.com>
2025-07-19 12:21:30 +0200

    funcion entero_a_romano segunda version

--- main.py
+++ main.py
@@ -19,12 +19,6 @@ X --->10
 V ---> 5
 I ---> 1
 '''
-diccionarios = {1000:"M", 500:"D", 100:"C", 50:"L", 10:"X", 5:"V", 1:"I"}
-
-unidades = {1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX"}
-decenas = {10:"X", 20:"XX", 30:"XXX", 40:"XL", 50:"L", 60:"LX", 70:"LXX", 80:"LXXX", 90:"XC"}
-centenas = {100:"C", 200:"CC", 300:"CCC", 400:"CD", 500:"D", 600:"DC", 700:"DCC", 800:"DCCC", 900:"CM"}
-millares = {1000: "M", 2000:"MM", 3000:"MMM"}
 
 dic_entero_a_romano = {
     1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX",
@@ -38,46 +32,32 @@ class RomanNumberError(Exception):
 
 #ingresar 1994 y retornar MCMXCIV
 def entero_a_romano(numero): #1994
+    numero = "{:0>4d}".format(numero)
     numero = str(numero) #transformando en cadena el valor 1994
+    '''
+    if len(numero) == 3:
+        numero = '0' + numero
+    elif len(numero) == 2:
+        numero = '00' + numero
+    elif len(numero) == 1:
+        numero = '000' + numero
+    '''
+
     numero_list = list(numero) #['1','9','9','4']
     #print(numero_list)
     valor_romano = ''
 
+
     cont = 0
     valor_num = 1000
-    while cont < 4:
+    while cont < len(numero_list):
         numero_list[cont] = int(numero_list[cont]) * valor_num
-        valor_romano += dic_entero_a_romano.get(numero_list[cont]) #buscar en el diccionario el valor en romano
+        valor_romano += dic_entero_a_romano.get(numero_list[cont],'') #buscar en el diccionario el valor en romano
         cont += 1
         valor_num /= 10
 
-    '''
-        for i in range(0, len(numero_list)): #[1000,900,90,4]
-            if i == 0:
-                numero_list[i] = int(numero_list[i])*1000 #agregar 000 al primer valor
-                valor_romano += millares.get(numero_list[i]) #buscar en el diccionario de millares el valor en romano
-            elif i == 1:
-                numero_list[i] = int(numero_list[i])*100
-                valor_romano += centenas.get(numero_list[i])
-            elif i == 2:
-                numero_list[i] = int(numero_list[i])*10
-                valor_romano += decenas.get(numero_list[i])
-            elif i == 3:
-                numero_list[i] = int(numero_list[i])*1
-                valor_romano += unidades.get(numero_list[i])
-    '''
-
-
-    #print(numero_list)
-
     return valor_romano
 
-print(entero_a_romano(1994))
+#print(entero_a_romano(1994))
 
-'''
-print(list('1994'))
-
-for i in '1994':
-    print(i)
-
-'''
+print(entero_a_romano(333))
--- pruebas_varias.py
+++ pruebas_varias.py
@@ -1,14 +1,65 @@
 diccionarios = {"jose":10, "carlos":23, "maria":33}
-print("carlos: ", diccionarios.get("carlos"))
+#print("carlos: ", diccionarios.get("carlos"))
 
 #for i in diccionarios.values():
 #for i in diccionarios:
-for c,v in diccionarios.items():
+#for c,v in diccionarios.items():
     #print(i)
     #print(diccionarios.get(i))
-    print(c+"-"+str(v))
+    #print(c+"-"+str(v))
 
-
+'''
 valor = 3
-valor = "{:0<4d}".format(3)
-print(valor)
+valor_obtenido = "{:0>4d}".format(valor)
+print(valor_obtenido)
+print(type(valor_obtenido))
+'''
+
+#repaso de diccionario
+d1 = {
+    "nombre": "Carlos",
+    "edad": 45,
+    "dni": "B27496583",
+    10: "X"
+}
+
+print(d1)
+
+d2 = (
+    dict[('nombre', 'Dulce'), ('edad',25), ('dni', '18469374C')]
+)
+
+print(d2)
+
+#funciones para diccionarios
+print('funcion get', d1.get(100, "No se encontro la clave"))
+
+#if d1.get(100) == None:
+#    print("No existe esta busqueda en el diccionario")
+
+#funcion clear, elimina todo el diccionario
+#d1.clear()
+#print("funcion clear", d1)
+
+#funcion de devolucion de items
+valores = d1.items()
+print(valores)
+
+for k,v in valores:
+    print(str(k)+' - '+ str(v))
+
+#obtener solo las claves
+llaves = d1.keys()
+
+#obtener solo los valores
+values = d1.values()
+
+#eliminar item especifico
+d1.pop(10)
+
+#para cargar nuevos valores a un diccionario
+d1.update({'a': 100, 'b': 20})
+print(d1)
+
+#para eliminar de manera aleatoria
+d1.popitem()
